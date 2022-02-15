d711fcf0868feba777bff4e164c4e37858d5d7f4
yanyangtian1106 <1035282452@qq.com>
2022-02-15 11:49:52 +0800

    函数的嵌套使用

---
+++ 函数的嵌套使用.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//void new_line()
+//{
+//	printf("hehe\n");
+//}
+//void three_line()
+//{
+//	int i = 0;
+//	for (i = 0; i < 3; i++)                                                     //函数的嵌套调用
+//	{
+//		new_line();
+//	}
+//}
+//int main()
+//{
+//	three_line();
+//	return 0;
+//}
