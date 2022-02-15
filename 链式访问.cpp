bd9a4c5b4e1d36f43a7b2e72e0e5d08b393a4af3
yanyangtian1106 <1035282452@qq.com>
2022-02-15 11:50:54 +0800

    链式访问

---
+++ 链式访问.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	strlen("abc");
+//	printf("%d\n",strlen("abc"));                                            //链式访问，把一个函数的返回值作为另一个函数的参数
+//	return 0;
+//}
