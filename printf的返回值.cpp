67c61874fe9a7714542e1a6b15389c3b65f6e65f
yanyangtian1106 <1035282452@qq.com>
2022-02-15 11:47:57 +0800

    printf的返回值的链式调用

---
+++ printf的返回值.cpp
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));                          //printf的返回值为打印字符个数所以该函数的结果为4321（链式访问）
+//	return 0;
+//}
