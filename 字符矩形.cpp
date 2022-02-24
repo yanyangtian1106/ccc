64a39b74774a4883059162d2a82907118df55e13
yanyangtian1106 <1035282452@qq.com>
2022-02-24 12:32:26 +0800

    字符矩形

---
+++ 字符矩形.cpp
@@ -0,0 +1,9 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//    char a;
+//    scanf("%c", &a);
+//    printf("%c%c%c\n%c%c%c\n%c%c%c", a, a, a, a, a, a, a, a, a);
+//    return 0;
+//}
