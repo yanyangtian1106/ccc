b3af0bbd8609a6098300641add983607bb385c62
yanyangtian1106 <1035282452@qq.com>
2022-02-24 12:33:28 +0800

    字符菱形

---
+++ 字符菱形.cpp
@@ -0,0 +1,9 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//    char a;
+//    scanf("%c", &a);
+//    printf("  %c\n %c%c%c\n%c%c%c%c%c\n %c%c%c\n  %c", a, a, a, a, a, a, a, a, a, a, a, a, a);
+//    return 0;
+//}
