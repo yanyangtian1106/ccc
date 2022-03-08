1e89d58db9e0b56d0827cf0ad8d8f222318ba1ea
yanyangtian1106 <1035282452@qq.com>
2022-03-08 12:10:27 +0800

    二维数组的结构

---
+++ 二维数组.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[3][4] = { {1,2,3} ,{4,5} };//表示三行四列
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		//打印二维数组里的数字需要找到其中对应的行列
+//		int j = 0;                                            //打印二维数组的每一个数字
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
