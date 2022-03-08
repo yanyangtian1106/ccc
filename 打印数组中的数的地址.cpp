5d67d7dc161eee7703fdd3c8df3103dc10caec49
yanyangtian1106 <1035282452@qq.com>
2022-03-08 12:09:30 +0800

    打印数组中每一个数的地址

---
+++ 打印数组中的数的地址.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10};
+//	int se = sizeof(arr) / sizeof(arr[0]);
+//	int i;
+//	for (i = 0; i < se; i++)                                              //打印数组中每一个数的地址
+//	{
+//		printf("&arr[%d]=%p\n", i, &arr[i]);
+//	}
+//	return 0;
+//}
