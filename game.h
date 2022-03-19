ad8574e51ce8ce4a34922404e4452fca955d5a21
yanyangtian1106 <1035282452@qq.com>
2022-03-19 21:19:50 +0800

    三子棋

--- game.h
+++
@@ -1,6 +0,0 @@
-#define ROW 3          //在头文件中定义之后以后需要改动参数直接在头文件中修改就可以修改测试文件中的全部
-#define COL 3          //使之更加方便
-#include <stdio.h>
-//函数申明
-void initboard(char board[ROW][COL], int row, int col);
-void displayboard(char board[ROW][COL], int row, int col);
