2c0f2eaf8fceb3a4dab204e2ab6c75c8e3741b20
yanyangtian1106 <1035282452@qq.com>
2022-03-28 19:43:14 +0800

    3.25作业笔记

---
+++ 作业笔记.cpp
@@ -0,0 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);                                                        //反逻辑操作符+1求负数绝对值
+//	printf("%d", ~a+1);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int x=1;
+//	int y=1;
+//	for (x = 1; x <= 50; x++)
+//	{
+//		for (y = 1; y <= 50; y++)                             //2题
+//		{ 
+//			if (x + y == 35 && 2 * x + 4 * y == 94)
+//				{
+//				printf("鸡的只数 = % d兔的只数 = % d", x, y);
+//				}
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	printf("%d %d %d", sizeof('x'), sizeof("x"), sizeof("\101"));                    
+//}
+
+
+//int main()
+//{
+//	printf("%f%%%%", 1.0 / 3);                                                           //%%的使用
+//}
+
+
+//int main()
+//{
+//	int a = 353;
+//	char b;
+//	b = a;
+//	printf("%c\n", b);
+//}
+
+
+//int main()
+//{
+//	char ch1, ch2;
+//	int n1, n2;
+//	ch1 = getchar();
+//	ch2 = getchar();                                                           //getchar()的使用一次只能取一个字符
+//	n1 = ch1 - '0';
+//	n2 = n1 * 10 + (ch2 - '0');
+//	printf("%d\n", n2);
+//}
+
+
+//int main()
+//{
+//	int a = 2;
+//	float b = 3.5;
+//	int c = 4;
+//	printf("%f",a*b/(c-1.5)+'a'%3);                                              //使用ascll码值的计算
+//}
+
+
+
+//int main()
+//{
+//	int x = -2345;
+//	float y = 12.3;
+//	printf("%6d,%06.2f", x, y);
+//}
+
+
+
+
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 1, k=2;
+//	/*k = (++a < 0) && !(b-- <= 0);*/                                   //前置++和后置++的区别
+//	printf("%d %d %d\n", ++a, b++, ++k);
+//}
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 5;
+//	int b = 4;
+//	int c = 6;
+//	int d;
+//	printf("%d\n" ,d=a>b?(a>c?a:c):(b));                           //条件操作符的使用
+//}
