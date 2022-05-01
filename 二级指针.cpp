a148fcf8baa42d3522d1585977151c659e412880
yanyangtian1106 <1035282452@qq.com>
2022-05-01 22:05:41 +0800

    二级指针

---
+++ 二级指针.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	//二级指针
+//	int a = 10;//a的类型为int
+//	int* pa = &a;//pa的类型为int*
+//	int** ppa = &pa;//ppa的类型为int**
+//	**ppa = 20;
+//	printf("%d\n", **ppa);
+//	printf("%d\n",*pa);
+//	printf("%d\n", a);
+//	return 0;
+//}
