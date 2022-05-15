e480f3fc57e8e72cbab6159ced99efd14558d255
yanyangtian1106 <1035282452@qq.com>
2022-05-15 15:43:28 +0800

    寻找素数

---
+++ 寻找素数.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int main()
+//{
+//	int i, k, c;
+//	c = 1;
+//	for (i = 1; i <= 100; i++)
+//	{                                                           //找100以内的素数
+//		for (k = 1; k <= i; k++)
+//		{
+//			if (i % k == 0 && k != 1 && i != k)
+//				c = 0;
+//		}
+//		if (c != 0)
+//			printf("%d\n", i);
+//		c = 1;
+//	}
+//	return 0;
+//}
