a505f5735a82fe453d451b9305a1547e92eeb004
yanyangtian1106 <1035282452@qq.com>
2022-09-03 17:55:31 +0800

    switch的使用

---
+++ switch的使用.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int day;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:                                                        //switch语句用法
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息日\n");
+//		break;                                                    //最后一个语句最好也加上break；
+//	default:                                                    //默认
+//		printf("输入错误\n");
+//
+//	}
+//	return 0;
+//}
