4edfcf0933024619eba77d92990d7cbaca2b7bb4
yanyangtian1106 <1035282452@qq.com>
2022-09-03 17:56:37 +0800

    打印Ascll码值

---
+++ 打印Ascll码.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int main()
+//{
+//  char arr[]={73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 , 33};
+//    int a=sizeof(arr)/sizeof(arr[0]);
+//    int b;
+//    for(b=0;b<a;b++)                                                       //打印ASCLL码 %c打印字符 %s打印字符串
+//    {
+//        printf("%c",arr[b]);
+//    }
+//    return 0;
+//}
+
+
+
+//    int main()
+//{
+//   char a;
+//   
+//    scanf("%c",&a);                                //数字转ASCLL码值，输入的是字符形式%c Ascll码值的序号，输出的是整型%d Ascll码值对应的东西
+//    printf("%d",a);
+//    return 0;
+//}
