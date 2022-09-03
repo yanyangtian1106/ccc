7c37b39b6d33a220a0eec32ff9ba461400dd46eb
yanyangtian1106 <1035282452@qq.com>
2022-09-03 17:53:41 +0800

    16进制与8进制控制前导显示

---
+++ 16进制与8进制的前导显示控制.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int main()
+//{
+//
+//
+//
+//	int a = 0XABCDEF;
+//	printf("%15d\n", a);                              //域宽
+//	printf("%#o %#X\n", 1234, 1234);                    //使用修饰符“#”控制前导显示,是控制显示，不是#0或者#0X return 0; }
+//	printf("%x\n", a);
+//	return 0;
+//}
