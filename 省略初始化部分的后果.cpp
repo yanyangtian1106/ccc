f328cb9745de7316961ddd6e26b0779130e06402
yanyangtian1106 <1035282452@qq.com>
2022-09-05 15:49:28 +0800

    省略for循环初始化部分的后果

---
+++ 省略初始化部分的后果.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)                                     //省略初始化部分后导致内部的for循环10次之后j=10
+//			                                                    //外部循环循环第二次时再进入内部循环时j=10，导致无法
+//			                                                    //继续打印hehe，从而使打印100个hehe变成了打印10个hehe
+//			                                                    //如果将初始化部分补上就能实现打印100个hehe
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
