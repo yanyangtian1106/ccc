888c10b4014eb3bdaf9db2d3c4d6f1843458f857
yanyangtian1106 <1035282452@qq.com>
2022-09-06 17:55:06 +0800

    求二维数组周边数值

---
+++ 求二维数组周边数值.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<conio.h>
+//#include<stdio.h>
+//#include<stdlib.h>
+//#define  M  4
+//#define  N  5
+//int fun(int a[M][N])
+//{
+//    int i = 0;
+//    int j = 0;
+//    int h = 0;
+//    int c = 0;
+//    for (i = 0; i < 4; i++)
+//    {
+//        for (j = 0; j < 5; j++)
+//        {
+//            h = h + a[i][j];
+//        }
+//    }
+//    for (i = 1; i < 3; i++)
+//    {
+//        for (j = 1; j < 4; j++)
+//        {
+//            c = c + a[i][j];
+//        }
+//    }
+//    return h - c;
+//}
+//int main()
+//{
+//    int y;
+//    int aa[M][N] = { {1,3,5,7,9},{2,9,9,9,4},{6,9,9,9,8},{1,3,5,7,0} };  //定义二维数组并赋初始值
+//
+//    y = fun(aa);
+//    printf("%d", y);
+//    return 0;
+//}
